dfd1fd5fae932070c46a6556b69053ee4498b590
KrakenMare <arcanuo123@gmail.com>
2022-05-30 20:46:52 +0000

    update navbar paths 2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,7 +59,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'intro',
+            docId: 'about',
             position: 'left',
             label: 'Docs',
           },
