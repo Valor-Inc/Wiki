5ad84f2caec0bc182d0330a90b0d41c948e635a7
KrakenMare <arcanuo123@gmail.com>
2022-07-25 01:07:35 +0000

    cleanup paths

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,7 +71,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'intro/getting-started',
+            docId: 'getting-started',
             position: 'left',
             label: 'Docs',
           },
