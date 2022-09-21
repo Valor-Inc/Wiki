a9870e819596e501b7e9dcfec2c4f9f3af4d28da
Deathmod <deathmodwikiman@gmail.com>
2022-09-20 21:43:34 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,7 +73,6 @@ const config = {
             docId: '/category/introduction',
             position: 'left',
             label: 'Valor Guides',
-          },
         },
         items: [
           {
