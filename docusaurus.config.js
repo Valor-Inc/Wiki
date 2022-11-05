ed9096acf98b86477c23fd119377c4de151d22fd
Rina-Valor <114798136+Rina-Valor@users.noreply.github.com>
2022-11-05 06:03:48 -0600

    Update docusaurus.config.js
    whoops

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -63,7 +63,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
         colorMode: {
-      defaultMode: 'light',
+      defaultMode: 'dark',
       disableSwitch: false,
       respectPrefersColorScheme: false,
     },
